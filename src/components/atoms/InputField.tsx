a93f4df159f2b66d9eff8b1c45b0775ff70bdda9
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-08-18 21:49:59 +0700

    add error ui in InputField

--- src/components/atoms/InputField.tsx
+++ src/components/atoms/InputField.tsx
@@ -1,41 +1,49 @@
 import { colors, spacing } from '@src/theme';
 import { forwardRef, useState } from 'react';
-import { StyleSheet, TextInput, TextInputProps } from 'react-native';
+import { StyleProp, StyleSheet, TextInput, TextInputProps, TextStyle, View } from 'react-native';
 
-import { CTEXT } from './CText';
+import { CTEXT, CText } from './CText';
 
 interface Props extends TextInputProps {
   width?: string | number | undefined;
   textCenter?: boolean;
+  styleInput?: StyleProp<TextStyle>;
+  error?: string;
 }
 
 export const InputField = forwardRef<TextInput, Props>(
-  ({ width, textCenter, style, ...otherProps }, ref) => {
+  ({ width, textCenter, style, styleInput, error, ...otherProps }, ref) => {
     const [isFocus, setIsFocus] = useState(false);
     return (
-      <TextInput
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-        ref={ref}
-        style={[
-          styles.base,
-          isFocus && styles.focus,
-          !!width && { width },
-          textCenter && styles.textCenter,
-          style,
-        ]}
-        selectionColor={colors.brand.primary}
-        {...otherProps}
-      />
+      <View style={[!!width && { width }, style]}>
+        <TextInput
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          ref={ref}
+          style={[
+            styles.base,
+            isFocus && styles.focus,
+            textCenter && styles.textCenter,
+            styleInput,
+          ]}
+          selectionColor={colors.brand.primary}
+          {...otherProps}
+        />
+        {!!error && (
+          <CText style={styles.errorText} variant="caption" color="error">
+            {error}
+          </CText>
+        )}
+      </View>
     );
   }
 );
 
 const styles = StyleSheet.create({
   base: {
+    minHeight: spacing[40],
     borderBottomWidth: 1,
     paddingHorizontal: spacing[4],
-    paddingBottom: spacing[4],
     fontSize: CTEXT.fontSize.h4,
     fontFamily: CTEXT.fontFamily.regular,
     color: colors.base.black,
@@ -46,5 +54,8 @@ const styles = StyleSheet.create({
   },
   textCenter: {
     textAlign: 'center',
+  },
+  errorText: {
+    marginTop: spacing[8],
   },
 });
