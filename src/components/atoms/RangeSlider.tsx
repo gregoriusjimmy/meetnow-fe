336fd4d4aeaaec4ab591ce1571da5cc697b8b96c
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-09-03 21:27:38 +0700

    fix rn-range-slider to use range-slider-react-native, add  axios

--- src/components/atoms/RangeSlider.tsx
+++ src/components/atoms/RangeSlider.tsx
@@ -1,7 +1,7 @@
 import { colors, spacing } from '@utils/theme';
 import { memo, useCallback } from 'react';
 import { View, StyleSheet, StyleProp } from 'react-native';
-import Slider from 'rn-range-slider';
+import Slider from 'range-slider-react-native';
 
 const Rail = memo(() => {
   return <View style={railStyles.root} />;
