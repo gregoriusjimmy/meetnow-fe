91d6dd9e8ebdf0aea806024be29df844a474daa3
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-09-06 16:05:00 +0700

    delete last name in NameScreen

--- src/features/sign-up/InputNameScreen.tsx
+++ src/features/sign-up/InputNameScreen.tsx
@@ -5,7 +5,6 @@ import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { TRootStackParamList } from '@src/AppNavigator';
 import { i18n } from '@utils/i18n';
-import { spacing } from '@utils/theme';
 import { useAtom } from 'jotai';
 import { useRef, useState } from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
@@ -16,31 +15,27 @@ import { firstNameAtom, lastNameAtom } from './atoms';
 type TInputNameScreenNavigationProp = NativeStackNavigationProp<TRootStackParamList, 'InputName'>;
 
 export function InputNameScreen() {
+  //TODO: refactor firstName + lastName to be Name
   const navigation = useNavigation<TInputNameScreenNavigationProp>();
   const lastNameInputRef = useRef<TextInput>(null);
   const [firstName, setFirstName] = useAtom(firstNameAtom);
-  const [lastName, setLastName] = useAtom(lastNameAtom);
-  const [errorFirstName, setErrorFirstName] = useState('');
+  const [, setLastName] = useAtom(lastNameAtom);
+  const [errorName, setErrorName] = useState('');
 
-  const handleChangeFirstName = (val: string) => {
+  const handleChangeName = (val: string) => {
     if (/[\s\d\W]/.test(val)) return;
     setFirstName(val);
   };
 
-  const handleChangeLastName = (val: string) => {
-    if (/[\s\d\W]/.test(val)) return;
-    setLastName(val);
-  };
-
   const handlePressContinue = () => {
     if (firstName.length < 3) {
-      setErrorFirstName(i18n.t('sign_up_first_name_min_error'));
+      setErrorName(i18n.t('sign_up_name_min_error'));
       return;
     }
-    setErrorFirstName('');
+    setErrorName('');
     setFirstName(firstName.charAt(0).toUpperCase() + firstName.slice(1));
-    setLastName(lastName.charAt(0).toUpperCase() + lastName.slice(1));
-    navigation.push('InputNickname');
+    setLastName(firstName);
+    navigation.push('InputUsername');
   };
 
   return (
@@ -50,30 +45,17 @@ export function InputNameScreen() {
       <View style={styles.content}>
         <InputField
           value={firstName}
-          onChangeText={handleChangeFirstName}
+          onChangeText={handleChangeName}
           autoFocus
-          style={styles.firstNameInput}
-          placeholder={i18n.t('sign_up_first_name_placeholder')}
+          placeholder={i18n.t('sign_up_name_placeholder')}
           onSubmitEditing={() => lastNameInputRef.current?.focus()}
           blurOnSubmit={false}
           maxLength={20}
-          autoComplete={'name-prefix'}
-          error={errorFirstName}
-        />
-
-        <InputField
-          ref={lastNameInputRef}
-          value={lastName}
-          onChangeText={handleChangeLastName}
-          placeholder={i18n.t('sign_up_last_name_placeholder')}
-          autoComplete={'name-suffix'}
+          autoComplete={'name'}
+          error={errorName}
         />
       </View>
-      <Button
-        disabled={!firstName || !lastName}
-        variant="primary"
-        size="l"
-        onPress={handlePressContinue}>
+      <Button disabled={!firstName} variant="primary" size="l" onPress={handlePressContinue}>
         {i18n.t('sign_up_continue')}
       </Button>
     </SignUpContainer>
@@ -84,8 +66,5 @@ const styles = StyleSheet.create({
   content: {
     justifyContent: 'center',
     flex: 0.9,
-  },
-  firstNameInput: {
-    marginBottom: spacing[40],
   },
 });
